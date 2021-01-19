e54cfd543929d2be506dc59bcd8b19ffb15453c8
Medhat Riad <medhat.riad@gmail.com>
2021-01-19 17:16:48 +0100

    Initial Commit

---
+++ blitzer/blitzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  blitzerApp.swift
+//  blitzer
+//
+//  Created by Medhat Riad on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct blitzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
